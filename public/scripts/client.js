f15af0eb9e68b3b194aa861286a30e149caf9d97
leightonchien <leightonchien@gmail.com>
2021-03-25 05:59:02 +0000

    createTweetElement: access provided tweets via elements

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,42 +4,75 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-    {
-      user: {
-        name: 'Newton',
-        avatars: 'https://i.imgur.com/73hZDYK.png',
-        handle: '@SirIsaac'
-      },
-      content: {
-        text:
-          'If I have seen further it is by standing on the shoulders of giants'
-      },
-      created_at: 1461116232227
-    },
-    {
-      user: {
-        name: 'Descartes',
-        avatars: 'https://i.imgur.com/nlhLi3I.png',
-        handle: '@rd'
-      },
-      content: {
-        text: 'Je pense , donc je suis'
-      },
-      created_at: 1461113959088
+const createTweetElement = tweetObj => {
+    //responsible for returning a tweet <article>
+    //Must contain entire HTML structure of the tweet
+    
+    const newTweet = 
+    `
+    <article>
+    <header class="tweet-head">
+      <div>
+        <img src=${tweetObj.user.avatars}/>
+        <span>${tweetObj.user.name}</span>
+      </div>
+      <div>
+        <span>${tweetObj.user.handle}</span>
+      </div>
+    </header>
+      <div>
+        <span>${tweetObj.content.text}</span>
+      </div>
+    <footer class="tweet-foot">
+      <div>
+        <span>${tweetObj.created_at}</span>
+      </div>
+      
+      <div class="tweet-reactions"> 
+        <span><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></span>
+      </div>
+    </footer>
+    </article>
+    `
+    return newTweet;
+  }
+  
+  const renderTweets = arrayOfTweetObj => {
+    //Need to append to .tweets-container
+    for (obj of arrayOfTweetObj) {
+      $('.tweet-container').append(createTweetElement(obj))
     }
-  ];
+  }
   
-  const renderTweets = function(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-  };
+  const ajaxPost = (url, data, callback) => {
+    $.post(url, data, callback);
+  }
   
-  const createTweetElement = function(tweet) {
-    let $tweet = $('<article>').addClass('tweet');
-    // ...
-    return $tweet;
-  };
+  const getTextLength = queryString => {
+    let text = '';
+    for (index in queryString) {
+      //text= is 5 chars long
+      if (index > 4) {
+        text += queryString[index];
+      }
+    }
+    return text.replace(/%20/g, " ").length;
+  }
   
-  renderTweets(data);
+  $(document).ready(function() {
+    //Want to make a GET request
+    const loadtweets = $.get('/tweets', function(data) {
+        renderTweets(data);
+    })
+  
+    //When the button is clicked, ie when the form is submitted
+    $('button').click(function(event) {
+      event.preventDefault();
+      const data = $('form').serialize()
+      console.log(getTextLength(data));
+  
+      const dataToPost = ajaxPost('/tweets', data, function() {
+        //console.log(data)
+      })
+    })
+  }) 

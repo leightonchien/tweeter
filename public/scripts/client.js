a0f955a081ac803a4ee6595a036e3cb08119bcd7
leightonchien <leightonchien@gmail.com>
2021-03-26 02:50:41 +0000

    eslinted and semi final for code review

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,122 +5,122 @@
  */
 
 const escape = function(str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
     
 
 
 const createTweetElement = tweetObj => {
-    //responsible for returning a tweet <article>
-    //Must contain entire HTML structure of the tweet
+  //responsible for returning a tweet <article>
+  //Must contain entire HTML structure of the tweet
     
-    const newTweet =
+  const newTweet =
     `
     <article>
-    <header class="tweet-head">
-      <div>
-        <img src=${tweetObj.user.avatars}/>
-        <span>${tweetObj.user.name}</span>
-      </div>
-      <div>
-        <span>${tweetObj.user.handle}</span>
-      </div>
-    </header>
+      <header class="tweet-head">
+        <div>
+          <img src=${tweetObj.user.avatars}/>
+          <span>${tweetObj.user.name}</span>
+        </div>
+        <div>
+          <span>${tweetObj.user.handle}</span>
+        </div>
+      </header>
       <div>
         <span>${escape(tweetObj.content.text)}</span>
       </div>
-    <footer class="tweet-foot">
-      <div>
-        <span>${moment(tweetObj.created_at).fromNow()}</span>
-      </div>
+      <footer class="tweet-foot">
+        <div>
+          <span>${moment(tweetObj.created_at).fromNow()}</span>
+        </div>
       
-      <div class="tweet-reactions"> 
-        <span><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></span>
-      </div>
-    </footer>
+        <div class="tweet-reactions"> 
+          <span><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></span>
+        </div>
+      </footer>
     </article>
     `;
-    return newTweet;
-  };
-  
-  const renderTweets = arrayOfTweetObj => {
-    //Need to prepend (to see latest tweets first) to .tweets-container
-    for (let obj of arrayOfTweetObj) {
-      $('.tweet-container').prepend(createTweetElement(obj));
-    }
-  };
+  return newTweet;
+};
 
-  const renderLastTweet = arrayOfTweetObj => {
-    const lastTweet = arrayOfTweetObj[arrayOfTweetObj.length - 1];
-    $('.tweet-container').prepend(createTweetElement(lastTweet));
-  };
-  
-  
-  const ajaxPost = (url, data, callback) => {
-    $.post(url, data, callback);
-  };
-  
-  const getText = queryString => {
-    let text = '';
-    for (let index in queryString) {
-      //text= is 5 chars long
-      if (index > 4) {
-        text += queryString[index];
-      }
-    }
-    return decodeURIComponent(text);
+const renderTweets = arrayOfTweetObj => {
+  //Need to prepend (to see latest tweets first) to .tweets-container
+  for (let obj of arrayOfTweetObj) {
+    $('.tweet-container').prepend(createTweetElement(obj));
+  }
+};
+
+const renderLastTweet = arrayOfTweetObj => {
+  const lastTweet = arrayOfTweetObj[arrayOfTweetObj.length - 1];
+  $('.tweet-container').prepend(createTweetElement(lastTweet));
 };
   
-  const resetErrorMessage = violation => {
-    if (violation === 'over count') {
-      $(".error-message").hide();
-      $(".error-message").empty();
-      $(".error-message").append("<p>Your tweet is too long!</p>");
-      $(".error-message").slideDown("slow");
-    } else if (violation === 'empty') {
-      $(".error-message").hide();
-      $(".error-message").empty();
-      $(".error-message").append("<p>Keep typing...</p>");
-      $(".error-message").slideDown("slow");
-    } else {
-      $(".error-message").hide();
-      $(".error-message").empty();
+  
+const ajaxPost = (url, data, callback) => {
+  $.post(url, data, callback);
+};
+  
+const getText = queryString => {
+  let text = '';
+  for (let index in queryString) {
+    //text= is 5 chars long
+    if (index > 4) {
+      text += queryString[index];
     }
-  };
+  }
+  return decodeURIComponent(text);
+};
+  
+const resetErrorMessage = violation => {
+  if (violation === 'over count') {
+    $(".error-message").hide();
+    $(".error-message").empty();
+    $(".error-message").append("<p>Your tweet is too long!</p>");
+    $(".error-message").slideDown("slow");
+  } else if (violation === 'empty') {
+    $(".error-message").hide();
+    $(".error-message").empty();
+    $(".error-message").append("<p>Keep typing...</p>");
+    $(".error-message").slideDown("slow");
+  } else {
+    $(".error-message").hide();
+    $(".error-message").empty();
+  }
+};
   
 
 //Re-size tweet textbox automatically while typing
-  $(document).ready(function() {
-    $('textarea').on('input', function () {
-        this.style.height = 'auto';
-        this.style.height = this.scrollHeight + 'px';
-      })
-      $.get('/tweets', renderTweets);
+$(document).ready(function() {
+  $('textarea').on('input', function() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+  });
+  $.get('/tweets', renderTweets);
   
-      // new tweet form set as hidden
-      $('.new-tweet').hide();
+  // new tweet form set as hidden
+  $('.new-tweet').hide();
 
-      // toggle button 1
-      $('.write').click(function(event) {
-      $('.new-tweet').slideToggle('slow');
-    });
+  // toggle write new tweet button
+  $('.write').click(function(event) {
+    $('.new-tweet').slideToggle('slow');
+  });
 
 
-    //When the button is clicked, ie when the form is submitted
-    $('.submit-and-counter button').click(function(event) {
+  //When the button is clicked, ie when the form is submitted
+  $('.submit-and-counter button').click(function(event) {
 
-      event.preventDefault();
+    event.preventDefault();
 
-      const data = $('form').serialize();
+    const data = $('form').serialize();
 
-      const dataLength = getText(data).length;
+    const dataLength = getText(data).length;
 
-      if (dataLength > 140) {
-        resetErrorMessage('over count');
+    if (dataLength > 140) {
+      resetErrorMessage('over count');
     } else if (dataLength === 0) {
-        resetErrorMessage('empty');
+      resetErrorMessage('empty');
     } else {
       resetErrorMessage();
 
@@ -128,19 +128,19 @@ const createTweetElement = tweetObj => {
         //Get the tweet that was just submitted
         $.get('/tweets', renderLastTweet);
 
-      //clear the box after tweets are posted
-      $('textarea').val("");
-    });
+        //clear the box after tweets are posted
+        $('textarea').val("");
+      });
 
-     //Reset the character counter to 140 after submitting the tweet
-     $(this)
-     .closest(".new-tweet")
-     .find(".counter")
-     .removeClass("negative-count")
-     .text(140);
+      //Reset the character counter to 140 after submitting the tweet
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .removeClass("negative-count")
+        .text(140);
 
-   }
-
-   });
+    }
 
   });
+
+});
